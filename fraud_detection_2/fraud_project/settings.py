cfb05520177b70d073f2be98ab7295a8dac419a9
soumya chimmalagi <soumyachimmalagi0304@gmail.com>
2026-04-16 07:06:33 +0530

    fix allowed hosts properly

--- fraud_detection_2/fraud_project/settings.py
+++ fraud_detection_2/fraud_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t&%444z9xa^hjjje-cki++0s0!1f+j&90hover)*b0xxvh)old'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fraud-detection-project-m7vv.onrender.com', '*']
 
 
 # Application definition
